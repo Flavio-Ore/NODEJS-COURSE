03dcbfe8f252114df46f56ee07d0788b85eeed30
Flavio-Ore <pseudominio.go@gmail.com>
2024-01-07 19:14:31 -0500

    feat: npm dependencies

--- npm/ls.js
+++ npm/ls.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
+const pc = require('picocolors')
 
 async function ls (folder = '') {
   let files
@@ -7,7 +8,7 @@ async function ls (folder = '') {
   try {
     files = await fs.readdir(folder)
   } catch (error) {
-    console.log(pc.red(`The ${folder} folder has not been read correctly.`))
+    console.log(pc.red(`x The ${folder} folder has not been read correctly.`))
     process.exit(1)
   }
 
@@ -15,9 +16,7 @@ async function ls (folder = '') {
     let stats
     const filePath = path.join(folder, file)
     try {
-      console.log('ANTES DE USAR TRYCATCH STAT')
       stats = await fs.stat(filePath)
-      console.log('DESPUES DE USAR TRYCATCH STAT')
     } catch (error) {
       console.log(`The ${filePath} file has not been read correctly.`)
       process.exit(1)
@@ -31,25 +30,28 @@ async function ls (folder = '') {
     const mDateTime = stats.mtime.toLocaleDateString().padStart(10)
     const mLocaleTime = stats.mtime.toLocaleTimeString() // modification time
 
-    return `${fileType} ${mDateTime.padStart(12)} ${mLocaleTime.padStart(
-      10
-    )} ${size.toString().padStart(15)} ${file.padStart(file.length + 1)}`
+    return `${pc.cyan(fileType)} ${pc.yellow(
+      mDateTime.padStart(12)
+    )} ${pc.yellow(mLocaleTime.padStart(10))} ${pc.green(
+      size.toString().padStart(15)
+    )} ${pc.blue(file.padStart(file.length + 1))}`
   })
 
   const filesInfo = await Promise.all(filePromises)
 
   const directory = `Directory: ${__dirname}`
 
-  console.log('\n')
-  console.log(`        ${directory}\n`)
+  console.log(`\n        ${directory}\n\n`)
 
   // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-  console.log('\x1b[32m') //FgGreen
+  // console.log('\x1b[32m') //FgGreen
 
-  console.log('Type   Date        Time                Size   Name')
   console.log(
-    '----  ----------   --------  --------------  ----------',
-    '\x1b[0m' //Reset console color (default)
+    pc.green(pc.bold('Type   Date        Time                Size   Name'))
+  )
+  console.log(
+    '----  ----------   --------  --------------  ----------'
+    // '\x1b[0m' //Reset console color (default)
   )
 
   filesInfo.forEach(fileInfo => console.log(fileInfo))
